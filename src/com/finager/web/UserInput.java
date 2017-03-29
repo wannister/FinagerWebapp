290dfe839e51b6c62255750babf0dec9b78830ec
JiuweiW <jiuweitongxue@gmail.com>
2017-03-29 15:28:28 -0400

    final double revised

--- src/com/finager/web/UserInput.java
+++ src/com/finager/web/UserInput.java
@@ -7,12 +7,11 @@ import java.util.*;
 
 import com.finager.model.*;; // notice this
 
-public class UserInput extends HttpServlet{
-	
-	//jsp page sample run
-	public void doPost(HttpServletRequest request, HttpServletResponse response) 
-						throws IOException, ServletException{
-		//combine all the preferences to an array of double
+public class UserInput extends HttpServlet {
+
+	// jsp page sample run
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		// combine all the preferences to an array of double
 		String p0 = request.getParameter("pref0");
 		String p1 = request.getParameter("pref1");
 		String p2 = request.getParameter("pref2");
@@ -26,7 +25,7 @@ public class UserInput extends HttpServlet{
 		String p10 = request.getParameter("pref10");
 		String p11 = request.getParameter("pref11");
 		String p12 = request.getParameter("pref12");
-		
+
 		List<Double> result = new ArrayList<Double>();
 		result.add(Double.parseDouble(p0));
 		result.add(Double.parseDouble(p1));
@@ -41,57 +40,60 @@ public class UserInput extends HttpServlet{
 		result.add(Double.parseDouble(p10));
 		result.add(Double.parseDouble(p11));
 		result.add(Double.parseDouble(p12));
-		
+
 		String prov = request.getParameter("prov");
 		Double yr = Double.parseDouble(request.getParameter("year"));
 		Double income = Double.parseDouble(request.getParameter("income"));
 		Double saving = Double.parseDouble(request.getParameter("saving"));
-		
-		//convert from vector to Double[]
+
+		// convert from vector to Double[]
 		Double[] input = new Double[13];
 		for (int i = 0; i < input.length; i++) {
 			input[i] = result.get(i);
 		}
-		
-		//TabGener run = new TabGener("Ontario",2016,50000,10000,input);
-		
-		/*      //Do not work
-		String f = "/WEB-INF/expenditure.csv";
-		File file = new File(f);
-		PrintWriter clear = new PrintWriter(f);
-		clear.close();*/
-		
-		
-		//Read the data from expenditure.csv
+
+		// TabGener run = new TabGener("Ontario",2016,50000,10000,input);
+
+		/*
+		 * //Do not work String f = "/WEB-INF/expenditure.csv"; File file = new
+		 * File(f); PrintWriter clear = new PrintWriter(f); clear.close();
+		 */
+
+		// Read the data from expenditure.csv
 		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
 		ServletContext cntxt = getServletContext();
 		String fName = "/WEB-INF/expenditure.csv";
 		InputStream ins = cntxt.getResourceAsStream(fName);
-		//create a variable to store the data
+		// create a variable to store the data
 		Vector<String> lines = new Vector<String>();
-		//start reading file
+		// start reading file
 		BufferedReader br = new BufferedReader((new InputStreamReader(ins)));
 		String word;
-		while((word= br.readLine())!= null){
+		while ((word = br.readLine()) != null) {
 			lines.add(word);
 		}
-		
-		TabGener run = new TabGener(lines,prov,yr,income,saving,input);
+
+		TabGener run = new TabGener(lines, prov, yr, income, saving, input);
 		List<String> block = new ArrayList<String>();
-		//iterate all the blocks
+		// iterate all the blocks
 		for (int i = 0; i < 13; i++) {
 			List temp_value = run.block(i);
 			List temp_string = run.Name(i);
-			//iterate through each blocks
+			// iterate through each blocks
 			for (int j = 0; j < temp_value.size(); j++) {
-				Double val = (Double) temp_value.get(j);
-				block.add(temp_string.get(j)+": "+val);
+				if (j == 0) {
+					String val = String.format("%.2f", temp_value.get(j));
+					block.add(temp_string.get(j) + ": " + val);
+					block.add("    ");
+				} else {
+					String val = String.format("%.2f", temp_value.get(j));
+					block.add(temp_string.get(j) + ": " + val);
+				}
 			}
 			block.add("---------------------");
 		}
 
-		
 		request.setAttribute("block", block);
 		RequestDispatcher view = request.getRequestDispatcher("table.jsp");
 		view.forward(request, response);
