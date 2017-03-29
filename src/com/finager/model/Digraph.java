cf442ab60f7b07f87ace804adec7be54ffe136b5
JiuweiW <jiuweitongxue@gmail.com>
2017-03-29 16:19:51 -0400

    final version add javadoc v1

--- src/com/finager/model/Digraph.java
+++ src/com/finager/model/Digraph.java
@@ -1,11 +1,18 @@
 package com.finager.model;
 
-
+/**
+ * 
+ * 
+ *
+ */
 public class Digraph {
 	private final int V;
 	private int E;
 	private Bag<Integer>[] adj;
-
+    /**
+     * 
+     * @param V
+     */
 	public Digraph(int V) {
 		this.V = V;
 		this.E = 0;
@@ -13,24 +20,41 @@ public class Digraph {
 		for (int v = 0; v < V; v++)
 			adj[v] = new Bag<Integer>();
 	}
-
+    /**
+     * 
+     * @return
+     */
 	public int V() {
 		return V;
 	}
-
+    /**
+     * 
+     * @return
+     */
 	public int E() {
 		return E;
 	}
-
+    /**
+     * 
+     * @param v
+     * @param w
+     */
 	public void addEdge(int v, int w) {
 		adj[v].add(w);
 		E++;
 	}
-
+    /**
+     * 
+     * @param v
+     * @return
+     */
 	public Iterable<Integer> adj(int v) {
 		return adj[v];
 	}
-
+    /**
+     * 
+     * @return
+     */
 	public Digraph reverse() {
 		Digraph R = new Digraph(V);
 		for (int v = 0; v < V; v++)
